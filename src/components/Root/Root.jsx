070fe5a202eb590433a88974a3dcf5f007001918
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-28 11:23:50 +0600

    root component added

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2>
+                this is root
+            </h2>
+        </div>
+    );
+};
+
+export default Root;
