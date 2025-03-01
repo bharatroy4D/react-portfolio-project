069594cce7ced95357a091bc341ef8ef6e42c6ef
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-03-01 17:48:46 +0600

    card into icon added

--- src/components/job/Job.jsx
+++ src/components/job/Job.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { AiOutlineDollar } from 'react-icons/ai';
+import { MdLocationOn } from "react-icons/md";
+
 
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary, } = job;
@@ -16,6 +19,14 @@ const Job = ({ job }) => {
                 <div className='text-start py-3'>
                     <button className='border border-blue-600 text-blue-600 font-bold rounded mr-3 px-5 py-2'>{remote_or_onsite}</button>
                     <button  className='border border-blue-600 text-blue-600 font-bold rounded mr-3 px-5 py-2'>{job_type}</button>
+                </div>
+                <div className='pb-3 flex gap-6 font-medium text-red-500'>
+                    <p className='flex
+                     gap-2 items-center'><MdLocationOn className='text-2xl'></MdLocationOn>
+                    {location}</p>
+                    <p className='flex
+                     gap-2 items-center'><AiOutlineDollar className='text-2xl'></AiOutlineDollar>
+                    Salary: {salary}</p>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
